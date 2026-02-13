3a0e94164e8e68154670d47efa5413292078fafb
Luis W <wingerterluis77@gmail.com>
2026-02-13 03:36:26 +0100

    new tests

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from src.traffic.io.parser import InputParser
 
 if __name__ == "__main__":
-    ip = InputParser("Beispielhausen.txt")
+    ip = InputParser("ShortRoads.txt")
     sim = ip.load_input()
     sim.run()
